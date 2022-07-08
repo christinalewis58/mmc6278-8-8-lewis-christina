d9621c2b2f36df92dc534d85228ec721a2c5f498
Christina Lewis <clewis13@ufl.edu>
2022-07-07 20:42:08 -0400

    declare variables for fetch

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code here
+//Declare global variables
+var URL = "https://api.openweathermap.org/data/2.5/weather"
+var weatherSection = document.getElementById('weather')
+var weatherSearch = document.getElementById('weather-search')
+var form = document.querySelector('form')
+
